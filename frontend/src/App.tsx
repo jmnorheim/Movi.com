0236a6c24357cca731d35338d87f9ec6dc65139e
bjorneme <bjorneme@stud.ntnu.no>
2023-10-03 14:32:42 +0200

    refactor: :recycle: Update folder structure pages
    Update folder structure pages
    
    #2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,19 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
-import MoviePage from './pages/MoviePage';
-import ProfilePage from './pages/ProfilePage';
-import MyLibraryPage from './pages/MyLibraryPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage/HomePage';
+import MoviePage from './pages/MoviePage/MoviePage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import MyLibraryPage from './pages/MyLibraryPage/MyLibraryPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 /**
  * Render the App component.
  * @returns {React.Component}
  */
 const App: React.FC = () => {
-  
-  const someMovieId: string = "123"; // Just an example. Replace with actual logic.
-
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        {/* Dynamically link to a specific movie by its ID */}
-        <Link to={`/movie/${someMovieId}`}>Specific Movie</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/my-library">My Library</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movie/:movieId" element={<MoviePage />} />
