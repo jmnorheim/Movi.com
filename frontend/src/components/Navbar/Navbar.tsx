32f9b7dc46c0bcc93eb6a2d1136b3e80dd169ab9
bjorneme <bjorneme@stud.ntnu.no>
2023-10-06 12:38:15 +0200

    feat: :sparkles: Implement Navbar
    Implement navbar with home, my-library and profile/login link.
    
    #3

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { AppBar, Toolbar, Box, IconButton, Typography } from '@mui/material';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+/**
+ * Render navbar.
+ *
+ * @returns {JSX.Element}
+ */
+const NavBar: React.FC = (): JSX.Element => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  
+  return (
+    <>
+      <AppBar position="static" className="app-bar" sx={{ backgroundColor: '#001F3F' }}>
+        <Toolbar className="toolbar">
+          <Box />
+          <Typography variant="h6" component={Link} to="/" className="movie-brand">Møvie</Typography>
+          <Box style={{ display: 'flex', gap: '16px' }}>
+            <IconButton color="inherit" component={Link} to="/my-library" className="icon-button">
+              <LibraryBooksIcon />
+              <Typography variant="caption">My Library</Typography>
+            </IconButton>
+            <IconButton color="inherit" component={Link} to={isLoggedIn ? "/profile" : "/login"} className="icon-button">
+              {isLoggedIn ? <AccountCircleIcon /> : <LoginIcon />}
+              <Typography variant="caption">{isLoggedIn ? 'Profile' : 'Login'}</Typography>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+export default NavBar;
