e5ecffcb9ab786b774af5cacec38fa9a6548f5b6
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-10-06 17:24:05 +0200

    feat: :sparkles: Add TanStack Query
    Add TanStack Query to the project and used useQuery in homePage to get the info
    
    #7

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import movieAPI from "../../services/movieAPI";
+import { useEffect, useState } from "react";
+import { MovieContent } from "../../interfaces";
+
 /**
  * Render the HomePage component.
  * @returns {React.FC}
  */
 const HomePage: React.FC = () => {
+  const [originals, setOriginals] = useState<MovieContent[]>([]);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["movies"],
+    queryFn: movieAPI,
+  });
+
+  useEffect(() => {
+    if (data && data instanceof Array) {
+      setOriginals(data);
+      console.log("setts data");
+    }
+  }, [data]);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return <div>Welcome to the Home Page</div>;
 };
 
--- frontend/src/services/movieAPI.ts
+++ frontend/src/services/movieAPI.ts
@@ -40,9 +40,48 @@ const movies: MovieContent[] = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
   },
+  {
+    imdbID: "tt0241527",
+    primaryTitle: "Harry Potter and the Sorcerer's Stone",
+    originalTitle: "Harry Potter and the Sorcerer's Stone",
+    isAdult: false,
+    startYear: 2001,
+    runtimeMinutes: 152,
+    genres: ["Adventure", "Family", "Fantasy"],
+    averageRating: 6.8,
+    totalVotes: 100420,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0304141",
+    primaryTitle: "Harry Potter and the Prisoner of Azkaban",
+    originalTitle: "Harry Potter and the Prisoner of Azkaban",
+    isAdult: false,
+    startYear: 2004,
+    runtimeMinutes: 142,
+    genres: ["Adventure", "Family", "Fantasy"],
+    averageRating: 3.5,
+    totalVotes: 69420,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0330373",
+    primaryTitle: "Harry Potter and the Goblet of Fire",
+    originalTitle: "Harry Potter and the Goblet of Fire",
+    isAdult: false,
+    startYear: 2005,
+    runtimeMinutes: 157,
+    genres: ["Adventure", "Family", "Fantasy"],
+    averageRating: 6.9,
+    totalVotes: 42069,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_SX300.jpg",
+  },
 ];
 
-const movieAPI = (): Promise<MovieContent | MovieContent[]> => {
+const movieAPI = (): Promise<MovieContent[]> => {
   return new Promise((resolve, reject) => {
     try {
       // const movie = movies[0];
