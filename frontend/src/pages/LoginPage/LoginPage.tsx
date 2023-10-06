19e5e42fc18c3437d1b29761fd6e9ab2f9167377
bjorneme <bjorneme@stud.ntnu.no>
2023-10-06 14:42:44 +0200

    feat: Create LoginPage and RegisterPage.
    Create LoginPage and RegisterPage.
    
    #16

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,59 @@
+import React from 'react';
+import { Button, TextField, Container, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
+
 /**
  * Render the LoginPage component.
- * @returns {React.Component}
+ * @returns {React.FC}
  */
-const LoginPage: React.FC = () => <div>Login to your account</div>;
+const LoginPage: React.FC = () => {
+  return (
+    <Container component="main" maxWidth="xs" className="login-container">
+      <Box className="login-box">
+
+        <Typography component="h1" variant="h5" className="login-title">
+          Login to your account
+        </Typography>
+
+        <Box component="form" noValidate className="login-form">
+          {/* Email input textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          {/* Password input textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          {/* Login button */}
+          <Button type="submit" fullWidth variant="contained" className="login-btn">
+            Login
+          </Button>
+
+          {/* Link to register page */}
+          <Box className="register-link-box">
+            <Typography variant="body2" className="register-link-text">
+              Don't have an account? <Link to="/register">Register</Link>
+            </Typography>
+          </Box>
+
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default LoginPage;
--- frontend/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,82 @@
+import React from 'react';
+import { Button, TextField, Container, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './RegisterPage.css';
+
 /**
- * Render the LoginPage component.
- * @returns {React.Component}
+ * Render the RegisterPage component.
+ * @returns {React.FC}
  */
-const RegisterPage: React.FC = () => <div>Register for a new account</div>;
+const RegisterPage: React.FC = () => {
+  return (
+    <Container component="main" maxWidth="xs" className="register-container">
+      <Box className="register-box">
+
+        <Typography component="h1" variant="h5" className="register-title">
+          Create your account
+        </Typography>
+
+        <Box component="form" noValidate className="register-form">
+
+          {/* Username textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+
+          {/* Email input textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+          />
+
+          {/* Password input textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+          />
+
+          {/* Confirm password input textfield */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required fullWidth name="confirm-password"
+            label="Confirm Password"
+            type="password"
+            id="confirm-password"
+            autoComplete="new-password"
+          />
+
+          {/* Register button */}
+          <Button type="submit" fullWidth variant="contained" className="register-btn">
+            Register
+          </Button>
+
+          {/* Link to login page */}
+          <Box className="login-link-box">
+            <Typography variant="body2" className="login-link-text">
+              Already have an account? <Link to="/login">Login</Link>
+            </Typography>
+          </Box>
+
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default RegisterPage;
