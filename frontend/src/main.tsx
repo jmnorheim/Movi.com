c29557a4e07d7463690b9cd67d140cea587727f6
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-10-07 15:00:13 +0200

    feat: :technologist: Add React Query Devtools
    Add React Query Devtools for better maniging the cache and other usfull things from the libary
    
    #18

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   </React.StrictMode>
 );
