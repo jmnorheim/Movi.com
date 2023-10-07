b6950ef5fa575f4cb81b3c34bf5f0dd4aa3c8626
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-10-07 15:54:45 +0200

    feat: :sparkles: Add connection of data between homePage and MovieContainerGrind
    Add connection of data between homePage and MovieContainerGrind throught the use of props, and and added loading if noe data is ready for the MovieContainerGrid
    
    #18 #13 #9 @DanielNHansen

--- frontend/src/components/movieContainerGrid/MovieContainerGrid.tsx
+++ frontend/src/components/movieContainerGrid/MovieContainerGrid.tsx
@@ -4,114 +4,11 @@ import MovieContainer from "../../components/movieContainer/MovieContainer";
 import { MovieContent } from "../../interfaces";
 import "./MovieContainerGrid.css";
 
-const movies: MovieContent[] = [
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0076759",
-    primaryTitle: "Star Wars: Episode IV - A New Hope",
-    originalTitle: "Star Wars",
-    isAdult: false,
-    startYear: 1977,
-    runtimeMinutes: 121,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 10,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0080684",
-    primaryTitle: "Star Wars: Episode V - The Empire Strikes Back",
-    originalTitle: "Star Wars: Episode V - The Empire Strikes Back",
-    isAdult: false,
-    startYear: 1980,
-    runtimeMinutes: 124,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 9.9,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0086190",
-    primaryTitle: "Star Wars: Episode VI - Return of the Jedi",
-    originalTitle: "Star Wars: Episode VI - Return of the Jedi",
-    isAdult: false,
-    startYear: 1983,
-    runtimeMinutes: 131,
-    genres: ["Action", "Adventure", "Fantasy"],
-    averageRating: 9.8,
-    totalVotes: 100000,
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-  },
-];
+interface MovieContainerGridProps {
+  movies: MovieContent[];
+}
 
-const MovieContainerGrid = () => {
+const MovieContainerGrid = ({ movies }: MovieContainerGridProps) => {
   return (
     <div className="MovieContainerGrid">
       {movies.map((movie) => (
