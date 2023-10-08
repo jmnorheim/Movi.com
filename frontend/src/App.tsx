35386dbda756bf5e86bd8e6217c124e5336e9082
bjorneme <bjorneme@stud.ntnu.no>
2023-10-08 20:09:30 +0200

    feat: :zap: Improve navbar to be more flexible.
    Improve navbar to be more flexible. Easier to use for phones.
    
    #3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage/HomePage';
-import MoviePage from './pages/MoviePage/MoviePage';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import MyLibraryPage from './pages/MyLibraryPage/MyLibraryPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
+import HomePage from "./pages/HomePage/HomePage";
+import MoviePage from "./pages/MoviePage/MoviePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import MyLibraryPage from "./pages/MyLibraryPage/MyLibraryPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
-import Navbar from './components/Navbar/Navbar';
-import { Container } from '@mui/material';
+import Navbar from "./components/Navbar/Navbar";
 
 /**
  * Render the App component.
@@ -17,18 +16,18 @@ import { Container } from '@mui/material';
 const App: React.FC = () => {
   return (
     <div className="App">
-        <Router basename="/project2" >
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movie/:movieId" element={<MoviePage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/my-library" element={<MyLibraryPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="*" element={<div>404 Not found </div>} />
-          </Routes>
-        </Router>
+      <Router basename="/project2">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie/:movieId" element={<MoviePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/my-library" element={<MyLibraryPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<div>404 Not found </div>} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { AppBar, Toolbar, Box, IconButton, Typography } from '@mui/material';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LoginIcon from '@mui/icons-material/Login';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import { useState } from "react";
+import { AppBar, Toolbar, Box, IconButton, Typography } from "@mui/material";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LoginIcon from "@mui/icons-material/Login";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 /**
  * Render navbar.
@@ -13,45 +13,64 @@ import './navbar.css';
  */
 const NavBar: React.FC = (): JSX.Element => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  
   return (
     <>
-      <AppBar position="static" className="app-bar" sx={{ backgroundColor: '#001F3F' }}>
+      <AppBar
+        position="static"
+        className="app-bar"
+        sx={{ backgroundColor: "#001F3F" }}
+      >
         <Toolbar className="toolbar">
           <Box />
 
           {/* "Movie" in middle of navbar */}
-          <Typography 
-            sx={{ 
-                position: 'absolute', 
-                top: '50%', 
-                left: '50%', 
-                transform: 'translate(-50%, -50%)' 
-            }} 
-            variant="h6" 
-            component={Link} 
-            to="/" 
-            className="movie-brand">
+          <Typography
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+            variant="h6"
+            component={Link}
+            to="/"
+            className="movie-brand"
+          >
             Movie
           </Typography>
-          
-          <Box style={{ display: 'flex', gap: '16px' }}>
+          <Box style={{ display: "flex", gap: "16px" }}>
             {/* My library icon with text */}
-            <IconButton color="inherit" component={Link} to="/my-library" className="icon-button">
-              <LibraryBooksIcon />
+            <IconButton
+              color="inherit"
+              component={Link}
+              to="/my-library"
+              className="icon-button"
+            >
+              <LibraryBooksIcon fontSize="small" />
               <Typography variant="caption">My Library</Typography>
             </IconButton>
 
             {/* Profile or Login icon with text */}
-            <IconButton color="inherit" component={Link} to={isLoggedIn ? "/profile" : "/login"} className="icon-button">
-              {isLoggedIn ? <AccountCircleIcon /> : <LoginIcon />}
-              <Typography variant="caption">{isLoggedIn ? 'Profile' : 'Login'}</Typography>
+            <IconButton
+              color="inherit"
+              component={Link}
+              to={isLoggedIn ? "/profile" : "/login"}
+              className="icon-button"
+            >
+              {isLoggedIn ? (
+                <AccountCircleIcon fontSize="small" />
+              ) : (
+                <LoginIcon />
+              )}
+              <Typography variant="caption">
+                {isLoggedIn ? "Profile" : "Login"}
+              </Typography>
             </IconButton>
           </Box>
         </Toolbar>
       </AppBar>
     </>
   );
-}
+};
 
 export default NavBar;
