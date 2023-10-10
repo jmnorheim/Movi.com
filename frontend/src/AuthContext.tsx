6d2de39eaa7600788a9968485844825c5f0094a0
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2023-10-10 19:36:57 +0200

    Revert "fix: :ambulance: Critical hotfix"
    This reverts commit 81a9367b0d82dc526fe76127662c29ca2e58258f.

--- frontend/src/AuthContext.tsx
+++ frontend/src/AuthContext.tsx
@@ -50,7 +50,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
    */
   const login = (email: string) => {
     localStorage.setItem("isLoggedIn", "true");
-    localStorage.setItem("email", email); // Store email in local storage
     setIsLoggedIn(true);
     setEmail(email);
     console.log(email);
@@ -61,7 +60,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
    */
   const logout = () => {
     localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("email"); // Remove email from local storage
     setIsLoggedIn(false);
     setEmail("");
   };
