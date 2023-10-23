3ed1fc6dcd6d4943097a53e8d96e3f263295af95
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-10-24 00:15:24 +0200

    feat: :sparkles: GQL Schema for movie, user & library
    GQL Schema for movie, user & library. Made structure for backend going forward, removed dead code
    
    #26

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
-import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { resolvers } from "../generated/type-graphql/index.js";
-import { buildSchema } from "type-graphql";
 import { PrismaClient } from "@prisma/client";
+import { readFileSync } from "fs";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql(readFileSync("../graphql/typeDefs.graphql", "utf-8"));
 
 const prisma = new PrismaClient();
 
@@ -16,13 +17,8 @@ const context: Context = {
 };
 
 async function main() {
-  const schema = await buildSchema({
-    resolvers,
-    validate: false,
-  });
-
   const server = new ApolloServer<Context>({
-    schema,
+    typeDefs,
   });
 
   const { url } = await startStandaloneServer(server, {
