f9b107727410e3d60ffd8f98f5aa2a443d2a7639
bjorneme <bjorneme@stud.ntnu.no>
2023-10-25 15:56:32 +0200

    feat: :sparkles: Implement resolver for users.
    Implement resolver for users and create helper functions to create simpler code.
    
    #34

--- backend/graphql/resolver/userResolver.ts
+++ backend/graphql/resolver/userResolver.ts
@@ -2,11 +2,51 @@ import { Resolvers } from "../../generated/resolvers-types";
 import { Context } from "../../src";
 
 /**
- * Resolvers for the User entity.
- * @type {Resolvers}
+ * Fetch detailed information of a user,
+ * including their libraries and favorite movies.
  */
+const getUserDetails = async (user: any, context: Context) => {
+    const libraries = await context.prisma.library.findMany({
+        where: { userID: user.userID }
+    });
+
+    const libraryMovie = await context.prisma.libraryMovie.findMany({
+        where: { libraryID: { in: libraries.map((lib) => lib.libraryID) } },
+        select: { imdbID: true }
+    });
+
+    const library = libraries.map(lib => ({
+        ...lib,
+        movies: libraryMovie.map(libmov => libmov.imdbID)
+    }));
+
+    const favorites = await context.prisma.userFavorites.findMany({
+        where: { userID: user.userID },
+        select: { imdbID: true }
+    });
+
+    return {
+        ...user,
+        library,
+        favorites: favorites.map(fav => fav.imdbID)
+    };
+};
+
+
 export const userResolver: Resolvers = {
     Query: {
+        /**
+         * Fetch a list of users with optional pagination.
+         */
+        users: async (_, { limit, offset }, context: Context) => {
+            const users = await context.prisma.user.findMany({
+                take: limit,   // number of records to take (limit)
+                skip: offset,  // number of records to skip (offset)
+            });
+
+            return Promise.all(users.map(user => getUserDetails(user, context)));
+        },
+
         /**
          * Fetch a user by their unique ID.
          */
@@ -14,32 +54,7 @@ export const userResolver: Resolvers = {
             const user = await context.prisma.user.findUnique({
                 where: { userID: userID },
             });
-
-            const libraries = await context.prisma.library.findMany({
-                where: { userID: user.userID }
-            });
-
-            const libraryMovie = await context.prisma.libraryMovie.findMany({
-                where: {libraryID: {in: libraries.map((lib) => lib.libraryID)}},
-                select: { imdbID: true }
-            });
-
-            const library =  libraries.map(lib => ({
-                ...lib,
-                movies: libraryMovie.map(libmov => libmov.imdbID)
-            }));
-
-
-            const favorites = await context.prisma.userFavorites.findMany({
-                where: { userID: user.userID },
-                select: { imdbID: true }
-            });
-
-            return {                                                                                            
-                ...user,
-                library,
-                favorites: favorites.map(fav => fav.imdbID) 
-            };
+            return getUserDetails(user, context);
         },
 
         /**
@@ -49,32 +64,7 @@ export const userResolver: Resolvers = {
             const user = await context.prisma.user.findUnique({
                 where: { email: email },
             });
-
-            const libraries = await context.prisma.library.findMany({
-                where: { userID: user.userID }
-            });
-
-            const libraryMovie = await context.prisma.libraryMovie.findMany({
-                where: {libraryID: {in: libraries.map((lib) => lib.libraryID)}},
-                select: { imdbID: true }
-            });
-
-            const library =  libraries.map(lib => ({
-                ...lib,
-                movies: libraryMovie.map(libmov => libmov.imdbID)
-            }));
-
-
-            const favorites = await context.prisma.userFavorites.findMany({
-                where: { userID: user.userID },
-                select: { imdbID: true }
-            });
-
-            return {                                                                                            
-                ...user,
-                library,
-                favorites: favorites.map(fav => fav.imdbID) 
-            };
+            return getUserDetails(user, context);
         }
-    },   
+    },
 };
