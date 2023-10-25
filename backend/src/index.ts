930f13cc26e78c07dbd56b0be953aa4acd730fc9
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2023-10-25 16:41:32 +0200

    refactor: :recycle: Clean up code
    Cleaned up code, added JSDocs to each query in movieResolver and removed unnecessary comments
    
    #32

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { PrismaClient } from "@prisma/client";
 import { typeDefs } from "../graphql/schema/index.js";
-import { Resolvers } from "../generated/resolvers-types.js";
 import { movieResolver } from "../graphql/resolver/movieResolver.js";
 
 const prisma = new PrismaClient();
@@ -14,27 +13,6 @@ export interface Context {
 const context: Context = {
   prisma: prisma,
 };
-
-// const resolvers: Resolvers = {
-//   Query: {
-//     users: async (_, __, context: Context) => {
-//       const users = await context.prisma.user.findMany();
-//       const fav = await context.prisma.userFavorites.findMany({
-//         where: { userID: { in: users.map((user) => user.userID) } },
-//       });
-//       console.log(fav);
-
-//       const tmp = users.map((user) => ({
-//         ...user,
-//         favorites: [],
-//         library: [],
-//       }));
-
-//       console.log(tmp);
-//       return tmp;
-//     },
-//   },
-// };
 
 async function main() {
   const server = new ApolloServer<Context>({
