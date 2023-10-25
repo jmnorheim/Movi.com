30876de732ce5d9b37241c51beb2628c7c6c0018
bjorneme <bjorneme@stud.ntnu.no>
2023-10-25 16:35:45 +0200

    feat: :sparkles: Implement Resolvers User.
    Implement Resolvers used to check is movie is in the users favorit list.
    
    #34

--- backend/graphql/resolver/userResolver.ts
+++ backend/graphql/resolver/userResolver.ts
@@ -2,24 +2,29 @@ import { Resolvers } from "../../generated/resolvers-types";
 import { Context } from "../../src";
 
 /**
- * Fetch detailed information of a user,
- * including their libraries and favorite movies.
+ * Helper function.
+ * 
+ * Fetch detailed information of a user, including libraries and favorite movies.
  */
 const getUserDetails = async (user: any, context: Context) => {
+    // Fetch users libraries
     const libraries = await context.prisma.library.findMany({
         where: { userID: user.userID }
     });
 
+    // Fetch movies in those libraries
     const libraryMovie = await context.prisma.libraryMovie.findMany({
         where: { libraryID: { in: libraries.map((lib) => lib.libraryID) } },
         select: { imdbID: true }
     });
 
+    // Library data
     const library = libraries.map(lib => ({
         ...lib,
         movies: libraryMovie.map(libmov => libmov.imdbID)
     }));
 
+    // Fetch users favorite movies
     const favorites = await context.prisma.userFavorites.findMany({
         where: { userID: user.userID },
         select: { imdbID: true }
@@ -32,6 +37,23 @@ const getUserDetails = async (user: any, context: Context) => {
     };
 };
 
+/**
+ * Helper function.
+ * 
+ * Check if a movie is in a user's favorite list.
+ */
+async function isMovieInFavorites(userID: string, imdbID: string, context: Context): Promise<boolean> {
+    // Fetch users favorite by movie ID
+    const favorite = await context.prisma.userFavorites.findFirst({
+        where: {
+            userID: userID,
+            imdbID: imdbID
+        }
+    });
+    // If the favorite exists, return true, else false
+    return !!favorite;
+}
+
 
 export const userResolver: Resolvers = {
     Query: {
@@ -43,7 +65,6 @@ export const userResolver: Resolvers = {
                 take: limit,   // number of records to take (limit)
                 skip: offset,  // number of records to skip (offset)
             });
-
             return Promise.all(users.map(user => getUserDetails(user, context)));
         },
 
@@ -65,6 +86,26 @@ export const userResolver: Resolvers = {
                 where: { email: email },
             });
             return getUserDetails(user, context);
+        },
+
+        /**
+         * Check if movie is in the user's favorite list by userID.
+         */
+        movieInFavoriteByUserID: async (_, { userID, imdbID }, context: Context) => {
+            const user = await context.prisma.user.findUnique({
+                where: { userID: userID }
+            });
+            return isMovieInFavorites(user.userID, imdbID, context);
+        },
+
+        /**
+         * Check if movie is in the users favorite list by email.
+         */
+        movieInFavoriteByUserEmail: async (_, { email, imdbID }, context: Context) => {
+            const user = await context.prisma.user.findUnique({
+                where: { email: email }
+            });
+            return isMovieInFavorites(user.userID, imdbID, context);
         }
-    },
+    }
 };
