d62417b45ab902b82576817cb43059840babd1fb
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2023-11-05 22:57:21 +0100

    feat: :sparkles: Add sending of newletter and social links
    Add sending of newletter and social links
    
    #46 @bjorneme @jmberget

--- frontend/src/components/newsletterBox/NewsLetterBox.tsx
+++ frontend/src/components/newsletterBox/NewsLetterBox.tsx
@@ -1,17 +1,80 @@
-import React from "react";
+import { useState } from "react";
 import "./NewsletterBox.css";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { sendEmail } from "../../services/utilities/mailHandler";
 
 const NewsLetterBox = () => {
+  const [dialogForm, setDialogForm] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleSendEmail = async () => {
+    try {
+      await sendEmail(email);
+      console.log(email);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="footerOuterContainer">
-      <div className="join-us-container">
-        <div className="text-wrapper">Join Us Today!</div>
-        <div className="text-wrapper-2">Description here</div>
-        <div className="div-wrapper">
-          <button className="join-now-button">Join Now</button>
+    <>
+      <div className="footerOuterContainer">
+        <div className="join-us-container">
+          <div className="text-wrapper">Join Us Today!</div>
+          <div className="text-wrapper-2">Description here</div>
+          <div className="div-wrapper">
+            <button
+              className="join-now-button"
+              onClick={() => setDialogForm(true)}
+            >
+              Join Now
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Dialog open={dialogForm} onClose={() => setDialogForm(false)}>
+        <DialogTitle>News Letter</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter your email to join our newsletter
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogForm(false)}>Cancel</Button>
+          <Button
+            onClick={() => {
+              handleSendEmail()
+                .then(() => setEmail(""))
+                .catch((err) => {
+                  console.log(err);
+                });
+              setDialogForm(false);
+            }}
+          >
+            Join
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
---
+++ frontend/src/services/utilities/mailHandler.ts
@@ -0,0 +1,18 @@
+// Declare the Email object provided by smtp.js
+declare const Email: any;
+
+export async function sendEmail(to: string) {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "prometheus.gruppe41@gmail.com",
+    Password: "EBBEEA44C1463CC77DB0F689A27091B933E0",
+    From: "prometheus.gruppe41@gmail.com",
+    To: to,
+    Subject: "This is the subject",
+    Body: "And this is the body for jens to see if it works :) ",
+    Port: 2525,
+  }).then((message: any) => {
+    Promise.resolve();
+    console.log(message);
+  });
+}
