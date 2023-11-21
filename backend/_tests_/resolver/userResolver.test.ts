c4e67b454bf6e49ff07ef3cbd2f6822b6fc5009b
bjorneme <bjorneme@stud.ntnu.no>
2023-11-21 16:13:29 +0100

    test: :white_check_mark: Test mutations in userResolver.ts.
    Test mutations in userResolver.ts. 100 % test coverage.
    
    #57

--- backend/_tests_/resolver/userResolver.test.ts
+++ backend/_tests_/resolver/userResolver.test.ts
@@ -3,7 +3,7 @@ import { userResolver } from "../../graphql/resolver/userResolver";
 import { Context } from "../../src";
 
 /**
- * Tessts queries and mutations in userResolver.
+ * Tests queries and mutations in userResolver.
  */
 describe("userResolver", () => {
     let mockContext;
@@ -12,42 +12,18 @@ describe("userResolver", () => {
      *  Sets up mocked methods for user.
      */
     beforeEach(() => {
-        mockContext = {
-            prisma: {
-                user: {
-                    findMany: vi.fn(),
-                    findUnique: vi.fn(),
-                    create: vi.fn(),
-                    update: vi.fn(),
-                    delete: vi.fn(),
-                },
-                library: {
-                    create: vi.fn(),
-                    delete: vi.fn(),
-                    findMany: vi.fn(),
-                },
-                libraryMovie: {
-                    findMany: vi.fn(),
-                },
-                userFavorites: {
-                    findMany: vi.fn(),
-                    findFirst: vi.fn(),
-                    create: vi.fn(),
-                    delete: vi.fn(),
-                },
-            },
-        };
+        mockContext = setupMockContext();
         vi.clearAllMocks();
     });
 
     /**
-     * Query tests.
+     * Querys
      */
     describe("Query", () => {
         /**
-         * Query: users - Fetches users with details.
+         * Query: users - Fetches users.
          */
-        it("Query: users - Fetches users with details.", async () => {
+        it("Query: users - Fetches users.", async () => {
             const mockUsers = [{ userID: "1", username: "User1" }];
             mockContext.prisma.user.findMany.mockResolvedValue(mockUsers);
             mockContext.prisma.library.findMany.mockResolvedValue([]);
@@ -91,7 +67,7 @@ describe("userResolver", () => {
         /**
          * Query: userByEmail - Fetches a user by their email.
          */
-        it("should fetch a user by their email", async () => {
+        it("Query: userByEmail - should fetch a user by their email", async () => {
             const mockUser = { email: "user1@example.com", username: "User1" };
             mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
             mockContext.prisma.library.findMany.mockResolvedValue([]);
@@ -112,7 +88,7 @@ describe("userResolver", () => {
         /**
          * Query: movieInFavoriteByUserID - Checks if a movie is in a user's favorite list by userID.
          */
-        it("Checks if a movie is in a user's favorite list by userID.", async () => {
+        it("Query: movieInFavoriteByUserID - Checks if a movie is in a user's favorite list by userID.", async () => {
             mockContext.prisma.user.findUnique.mockResolvedValue({ userID: "1", email: "user1@example.com" });
             mockContext.prisma.userFavorites.findFirst.mockResolvedValue({ userID: "1", imdbID: "movie1" });
 
@@ -141,4 +117,235 @@ describe("userResolver", () => {
             expect(result).toBe(true);
         });
     });
+
+    /**
+     * Mutation tests
+     */
+    describe("Mutations", () => {
+        /**
+         * Mutation: createUser - Should create a new user.
+         */
+        it("Mutation: createUser - Should create a new user.", async () => {
+            const newUser = { username: 'NewUser', email: 'newuser@example.com', password: "Password" };
+            mockContext.prisma.user.create.mockResolvedValue(newUser);
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.createUser(null, newUser, mockContext as unknown as Context);
+    
+            expect(mockContext.prisma.user.create).toHaveBeenCalledWith({ data: newUser });
+            expect(result).toEqual(expect.objectContaining({
+                username: 'NewUser',
+                email: 'newuser@example.com',
+                library: [],
+                favorites: [],
+            }));
+        });
+
+        /**
+         * Mutation: updateUser - Should update a user.
+         */
+        it("Mutation: updateUser - Should update a user", async () => {
+            const updatedUserData = { username: "UpdatedUser", email: "updatedUser@example.com", password: "newPassword" };
+            const updatedUser = { userID: '1', ...updatedUserData };
+            mockContext.prisma.user.update.mockResolvedValue(updatedUser);
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.updateUser(null, { userID: "1", ...updatedUserData }, mockContext as unknown as Context);
+    
+            expect(mockContext.prisma.user.update).toHaveBeenCalledWith({
+                where: { userID: "1" },
+                data: updatedUserData
+            });
+            expect(result).toEqual(expect.objectContaining({
+                userID: "1",
+                username: "UpdatedUser",
+                email: "updatedUser@example.com",
+                library: [],
+                favorites: [],
+            }));
+        });
+
+        /**
+         * Mutation: deleteUser - Should delete a user.
+         */
+        it("Mutation: deleteUser - Should delete a user", async () => {
+            const userToDelete = { userID: "1", username: "User1" };
+            mockContext.prisma.user.findUnique.mockResolvedValue(userToDelete);
+            mockContext.prisma.user.delete.mockResolvedValue(userToDelete);
+
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.deleteUser(null, { userID: "1" }, mockContext as unknown as Context);
+    
+            expect(mockContext.prisma.user.findUnique).toHaveBeenCalledWith({ where: { userID: "1" } });
+            expect(mockContext.prisma.user.delete).toHaveBeenCalledWith({ where: { userID: "1" } });
+            expect(result).toEqual(expect.objectContaining(userToDelete));
+        });
+        
+        /**
+         * Mutation: addMovieToFavorite - Should add a movie to a user's favorite.
+         */
+        it("Mutation: addMovieToFavorite - Should add a movie to a user's favorites", async () => {
+            const userID = "1";
+            const imdbID = "movie1";
+            const mockUser = { userID, username: "User1", email: "user1@example.com" };
+    
+            mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
+            mockContext.prisma.userFavorites.create.mockResolvedValue({ userID, imdbID });
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.addMovieToFavorite(null, { userID, imdbID }, mockContext as unknown as Context);
+
+            expect(result).toEqual(expect.objectContaining({
+                userID,
+                email: "user1@example.com",
+            }));
+        });
+
+        /**
+         * Mutation: removeMovieFromFavorite - Should add a movie to a user's favorite.
+         */
+        it("Mutation: removeMovieFromFavorite - Should remove a movie from a user's favorites", async () => {
+            const userID = "1";
+            const imdbID = "movie1";
+            const mockUser = { userID, username: "User1", email: "user1@example.com"};
+    
+            mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
+            mockContext.prisma.userFavorites.delete.mockResolvedValue({ userID, imdbID });
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.removeMovieFromFavorite(null, { userID, imdbID }, mockContext as unknown as Context);
+
+            expect(result).toEqual(expect.objectContaining({
+                userID,
+                email: "user1@example.com",
+            }));
+        });
+
+        /**
+         * Mutation: addLibrary - Should add a new library for the user.
+         */
+        it("Mutation: addLibrary - Should add a new library for the user", async () => {
+            const userID = "1";
+            const libraryName = "New Library";
+            const mockUser = { userID, username: "User1", email: "user1@example.com"};
+    
+            mockContext.prisma.library.create.mockResolvedValue({ userID, name: libraryName });
+            mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
+            mockContext.prisma.library.findMany.mockResolvedValue([{ userID, name: libraryName }]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.addLibrary(null, { userID, libraryName }, mockContext as unknown as Context);
+
+            expect(result).toEqual(expect.objectContaining({
+                userID,
+                email: "user1@example.com",
+                library: expect.arrayContaining([
+                    expect.objectContaining({
+                        name: libraryName,
+                        userID: expect.any(String), 
+                        movies: expect.any(Array),
+                    }),
+                ]),
+                favorites: expect.any(Array),
+            }));
+        });
+        
+        /**
+         * Mutation: addLibrary - Should remove a library based on libraryID.
+         */
+        it("Mutation: addLibrary - Should remove a library based on libraryID", async () => {
+            const userID = "1";
+            const libraryID = "lib1";
+            const mockUser = { userID, username: "User1", email: "user1@example.com" };
+    
+            mockContext.prisma.library.delete.mockResolvedValue({ libraryID });
+            mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
+
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.removeLibrary(null, { userID, libraryID }, mockContext as unknown as Context);
+    
+            expect(mockContext.prisma.library.delete).toHaveBeenCalledWith({ where: { libraryID } });
+            expect(result).toEqual(expect.objectContaining({
+                userID,
+                email: "user1@example.com",
+                library: [],
+                favorites: [],
+            }));
+        });
+
+        /**
+         * Mutation: removeLibrary - Should remove a library based on libraryID.
+         */
+        it("Mutation: removeLibrary - Should remove a library based on libraryID", async () => {
+            const userID = "1";
+            const libraryID = "lib1";
+            const mockUser = { userID, username: "User1", email: "user1@example.com" };
+    
+            mockContext.prisma.library.delete.mockResolvedValue({ libraryID });
+            mockContext.prisma.user.findUnique.mockResolvedValue(mockUser);
+
+            mockContext.prisma.library.findMany.mockResolvedValue([]);
+            mockContext.prisma.libraryMovie.findMany.mockResolvedValue([]);
+            mockContext.prisma.userFavorites.findMany.mockResolvedValue([]);
+    
+            const result = await userResolver.Mutation.removeLibrary(null, { userID, libraryID }, mockContext as unknown as Context);
+    
+            expect(mockContext.prisma.library.delete).toHaveBeenCalledWith({ where: { libraryID } });
+            expect(result).toEqual(expect.objectContaining({
+                userID,
+                email: "user1@example.com",
+                library: [],
+                favorites: [],
+            }));
+        });
+    });
 });
+
+// Helper functions ===============================================================
+
+/**
+ *  Sets up mocked methods for user.
+ */
+const setupMockContext = () => {
+    return {
+        prisma: {
+            user: {
+                findMany: vi.fn(),
+                findUnique: vi.fn(),
+                create: vi.fn(),
+                update: vi.fn(),
+                delete: vi.fn(),
+            },
+            library: {
+                create: vi.fn(),
+                delete: vi.fn(),
+                findMany: vi.fn(),
+            },
+            libraryMovie: {
+                findMany: vi.fn(),
+            },
+            userFavorites: {
+                findMany: vi.fn(),
+                findFirst: vi.fn(),
+                create: vi.fn(),
+                delete: vi.fn(),
+            },
+        },
+    };
+}
+
