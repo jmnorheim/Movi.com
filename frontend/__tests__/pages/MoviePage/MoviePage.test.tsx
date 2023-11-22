b2f5327fec39289c79a6e5d2579f384a56cca001
bjorneme <bjorneme@stud.ntnu.no>
2023-11-22 22:19:34 +0100

    test: :white_check_mark: Add test MoviePage.tsx
    Add test MoviePage.tsx.  Test that "Add To Library" button is rendered.
    
    #49

--- frontend/__tests__/pages/MoviePage/MoviePage.test.tsx
+++ frontend/__tests__/pages/MoviePage/MoviePage.test.tsx
@@ -1,8 +1,78 @@
-import { expect, it, suite } from "vitest";
+import React from "react";
+import { render as rtlRender, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { describe, it, vi, expect } from "vitest";
+import LibraryPage from "../../../src/pages/LibraryPage/LibraryPage";
+import { AuthProvider } from "../../../src/services/auth/AuthContext";
+import MoviePage from "../../../src/pages/MoviePage/MoviePage";
 
-// Making sure the test suite is working
-suite("Initial Test", () => {
-  it("should always be true", () => {
-    expect(true).toBeTruthy();
+/**
+ * @vitest-environment jsdom
+ */
+
+/** Overrides default window alert with mock function. */
+window.alert = vi.fn();
+
+/**
+ * getMovies.ts`: Mocks `useMoviesInByLibraryIDQuery` to provide predefined movie data.
+ */
+vi.mock("../../../src/services/getMovies.ts", () => ({
+  useMovie: () => ({ data: mockMovie }),
+}));
+
+/**
+ * AuthContext.tsx`: Mocks `useAuth` to simulate an authenticated user context.
+ */
+vi.mock("../../../src/services/auth/AuthContext.tsx", async () => {
+  const actual = await vi.importActual(
+    "../../../src/services/auth/AuthContext.tsx"
+  );
+  return {
+    ...actual,
+    useAuth: () => ({ userID: "user123", isAuthenticated: true }),
+  };
+});
+
+// Also defines `mockMovies`, an array of mock movie data used in tests.
+const mockMovie = {
+  imdbID: "tt0111161",
+  primaryTitle: "The Shawshank Redemption",
+  averageRating: 9.3,
+  runtimeMinutes: 142,
+  poster: "https://example.com/poster/shawshank.jpg",
+};
+
+/** QueryClient instance */
+const queryClient = new QueryClient();
+
+/**
+ * Render component with necessary providers.
+ * @param {React.ReactElement} ui
+ */
+function render(ui: React.ReactElement, { route = "/movies/tt0111161" } = {}) {
+  return rtlRender(
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <MemoryRouter initialEntries={[route]}>
+          <Routes>
+            <Route path="/movies/:movieId" element={ui} />
+          </Routes>
+        </MemoryRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+/**
+ * Tests MoviePage.
+ */
+describe("MoviePage Component", () => {
+  /**
+   * Tests rendering of movies.
+   */
+  it("renders 'Add To Library' on MoviePage", async () => {
+    render(<MoviePage />, { route: "/movies/tt0111161" }); // Pass the test route
+    expect(screen.getByText("Add To Library")).toBeDefined();
   });
 });
--- frontend/src/pages/MoviePage/MoviePage.tsx
+++ frontend/src/pages/MoviePage/MoviePage.tsx
@@ -70,7 +70,10 @@ const MoviePage: React.FC = () => {
     }
   }, [recommendedData, movieId]);
 
-  const printGenresNicely = (genres: string[]) => {
+  const printGenresNicely = (genres: string[] | undefined) => {
+    if (!Array.isArray(genres)) {
+      return ""; // or a default string
+    }
     let genresString = "";
     genres.forEach((genre: string) => {
       genresString += genre + ", ";
