98da0e0b737da9fc26a0fdaeb9687d3b22cf84f6
bjorneme <bjorneme@stud.ntnu.no>
2023-11-28 19:22:02 +0100

    test: :white_check_mark: Create e2e test to HomePage and ProfilePage
    Create e2e test to HomePage and ProfilePage
    
    #53

--- cypress/e2e/LoginPage.spec.cy.ts
+++ cypress/e2e/LoginPage.spec.cy.ts
@@ -1,41 +1,43 @@
 /**
  * Cypress tests for the LoginPage.
  */
-describe('Login Page', () => {
-    /**
-     * Before each test, visit the login page.
-     */
-    beforeEach(() => {
-      cy.visit('http://it2810-29.idi.ntnu.no/project2/login');
-    });
-  
-    /**
-     * Test error message when input field for password and email is empty.
-     */
-    it('error message when the input fields are empty', () => {
-      cy.get('.login-form').contains('Login').click();
-      cy.get('.login-form').should('contain', 'Email is required');
-      cy.get('.login-form').should('contain', 'Password is required');
-    });
-
-    /**
-     * Test unsuccessful login process.
-     */
-    it('test unsuccesfull login', () => {
-        cy.get('[id=email]').type('newuserLoginTest@example.com');
-        cy.get('[id=password]').type('wrongpassword');
-        cy.get('.login-form').contains('Login').click();
-        cy.get('.error-login').should('contain', 'Invalid email or password.');
-    });
-    
-    /**
-     * Test successful login process. Then navigate to profile page.
-     */
-    it('navigates to /profile on successful login', () => {
-      cy.get('[id=email]').type('newuserLoginTest@example.com');
-      cy.get('[id=password]').type('Password123!');
-      cy.get('.login-form').contains('Login').click();
-      cy.url().should('include', '/profile');
-    });
+describe("LoginPage and log out functionality ProfilePage", () => {
+  /**
+   * Before each test, visit the login page.
+   */
+  beforeEach(() => {
+    cy.visit("http://it2810-29.idi.ntnu.no/project2/login");
   });
-  
+
+  /**
+   * Test error message when input field for password and email is empty.
+   */
+  it("error message when the input fields are empty", () => {
+    cy.get(".login-form").contains("Login").click();
+    cy.get(".login-form").should("contain", "Email is required");
+    cy.get(".login-form").should("contain", "Password is required");
+  });
+
+  /**
+   * Test unsuccessful login process.
+   */
+  it("test unsuccesfull login", () => {
+    cy.get("[id=email]").type("newuserLoginTest@example.com");
+    cy.get("[id=password]").type("wrongpassword");
+    cy.get(".login-form").contains("Login").click();
+    cy.get(".error-login").should("contain", "Invalid email or password.");
+  });
+
+  /**
+   * Test successful login process. Then navigate to profile page.
+   */
+  it("navigates to /profile on successful login", () => {
+    cy.get("[id=email]").type("newuserLoginTest@example.com");
+    cy.get("[id=password]").type("Password123!");
+    cy.get(".login-form").contains("Login").click();
+    cy.url().should("include", "/profile");
+
+    //Test logout button on profile page
+    cy.get(".button").contains("Log Out").click();
+  });
+});
