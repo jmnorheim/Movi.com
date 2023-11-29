517d811a55740acbfa49b6ce55a0b9c155d7d80a
bjorneme <bjorneme@stud.ntnu.no>
2023-11-29 02:08:05 +0100

    test: :white_check_mark: Create unittests SortMenu.
    Create unittests SortMenu.
    
    #49

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ["text", "html"],
+      exclude: [
+        "**/src/components/newsletterBox/NewsLetterBox.tsx",
+        "**/src/services/utilities/mailHandler.ts",
+      ],
     },
   },
 });
